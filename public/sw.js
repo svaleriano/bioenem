43eb849316763d43a2de5722cd20d947280d72ee
svaleriano <svaleriano@live.com>
2026-04-25 15:11:20 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'bioenem-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
